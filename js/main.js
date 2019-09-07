613b4351ea1b0610127577e047c06451b70a8bf6
luis <valdivialuis1989@gmail.com>
2019-09-07 00:06:05 -0500

    phaser methods

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 window.onload = function(){
-	var game = new Phaser.Game(640,960,Phaser.AUTO)
-	console.log("iniciamos Phaser");
+	var game = new Phaser.Game(640,960,Phaser.AUTO,'',{
+		preload:preload,
+		create:create,
+		update:update
+	});
+
+	function preload(){
+
+	}
+
+	function create(){
+
+	}
+
+	function update(){
+
+	}
+	
 }

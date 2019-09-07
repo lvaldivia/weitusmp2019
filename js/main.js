96178773e61341a3a003f9f8f765ad229235b215
luis <valdivialuis1989@gmail.com>
2019-09-07 00:08:18 -0500

    loading images

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 window.onload = function(){
-	var game = new Phaser.Game(640,960,Phaser.AUTO,'element',{
+	var game = new Phaser.Game(640,960,Phaser.AUTO,'',{
 		preload:preload,
 		create:create,
 		update:update
 	});
 
 	function preload(){
-
+		game.load.image("background","img/background.png");
+		game.load.image("candy","img/candy.png");
+		game.load.image("floor","img/floor.png");
+		game.load.image("gameover","img/gameover.png");
+		game.load.image("monster-iddle","img/monster-iddle.png");
+		game.load.image("score-bg","img/score-bg.png");
+		game.load.image("title","img/title.png");
 	}
 
 	function create(){
